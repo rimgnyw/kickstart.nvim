1206551822b1552d9b0559253fd89d9a16dc5ecd
rimgnyw <94633794+rimgnyw@users.noreply.github.com>
2024-09-10 23:47:05 +0200

    remove unused lsps

--- init.lua
+++ init.lua
@@ -613,9 +613,9 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {},
+        -- pyright = {},
         -- rust_analyzer = {},
-        csharp_ls = {},
+        -- csharp_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
