a5044ee2249b5327040d7d97dfe04b50115427cb
rimgnyw <markus.wingmyr@telia.com>
2024-09-19 13:27:18 +0200

    remove scamware

--- init.lua
+++ init.lua
@@ -621,7 +621,6 @@ require('lazy').setup({
         -- rust_analyzer = {},
         csharp_ls = {},
         --
-        ltex = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
