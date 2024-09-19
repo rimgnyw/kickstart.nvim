c567dd4db6646c1632846bde0507ff5c2dc63a3d
rimgnyw <markus.wingmyr@telia.com>
2024-09-19 14:33:46 +0200

    add completion for latex

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,19 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.maplocaleader = ' '
+    end,
+  },
+  {
+    'micangl/cmp-vimtex',
+  },
+}
