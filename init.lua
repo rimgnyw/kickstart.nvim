3d2885297daf7c6f4095c6ae58a2f7ae82428515
Markus Wingmyr <wingmyr@kth.se>
2025-01-17 14:45:18 +0100

    add haskell lsp

--- init.lua
+++ init.lua
@@ -677,8 +677,9 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
-        csharp_ls = {},
+        -- csharp_ls = {},
         --
+        hls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
